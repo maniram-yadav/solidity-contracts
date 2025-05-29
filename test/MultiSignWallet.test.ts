d8dc7699e5a5efc84d6b5c0d6c9dd28f06325554
maniram <maniramyadav786@gmail.com>
2025-05-29 16:46:01 +0000

    log parsing implementation

--- test/MultiSignWallet.test.ts
+++ test/MultiSignWallet.test.ts
@@ -4,22 +4,35 @@ import { MultiSignWallet } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("MultiSignWallet Tests", function () {
-  let multiSignWallet: MultiSignWallet ;
+  let multiSignWallet: MultiSignWallet;
   let owner: HardhatEthersSigner;
 
   beforeEach(async function () {
     const owners = await ethers.getSigners();
     const MultiSignWalletFactory = await ethers.getContractFactory("MultiSignWallet");
-    multiSignWallet = await MultiSignWalletFactory.deploy([...owners],3);
+    multiSignWallet = await MultiSignWalletFactory.deploy([...owners], 3);
     await multiSignWallet.waitForDeployment();
   });
 
-  it("get owners",async function() {
-    const owners = await  multiSignWallet.getOwners();
-    
-    console.log(owners.length)
-    // expect(owners.length).to.be.
+  it("get owners", async function () {
+    const owners = await multiSignWallet.getOwners();
+    expect(owners.length).to.be.equal(20)
+  });
 
+  it("Submit Transaction", async function () {
+    const owners = await multiSignWallet.getOwners();
+    const tx = await multiSignWallet.submitTransaction(owners[2], 1, "0x");
+    const receipt = await tx.wait();
+    const logs = receipt?.logs;
+    const log = logs ? logs[0] : null;
+    const iface = new ethers.Interface(multiSignWallet.interface.fragments);
+    const parsedLog = log && iface.parseLog(log);
+    console.log(parsedLog)
+    console.log(parsedLog?.fragment.name)
+    console.log(parsedLog?.args)
+    expect(parsedLog?.args[2]).to.be.equal(owners[2])
+    expect(parsedLog?.args[3]).to.be.equal(1)
+    expect(parsedLog?.args[4]).to.be.equal("0x")
   });
 
 });
